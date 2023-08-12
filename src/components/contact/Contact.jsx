a5e95353be7f0651d721c50bd3d5ea743ba84311
Ydv Hiten <ydvhiten@gmail.com>
2023-08-12 15:56:38 +0530

    commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
             <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>ydvhiten@gmail.com</h5>
-            <a href="mailto:mekomerikomg@gmail.com">Send a message</a>
+            <a href="mailto:ydvhiten@gmail.com">Send a message</a>
           </article>
         </div>
         <form ref={formRef} onSubmit={handleSubmit}>
