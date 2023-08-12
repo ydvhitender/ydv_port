d20bf9d115513a2ce096ca86487f26ec4bf0fdd2
Ydv Hiten <ydvhiten@gmail.com>
2023-08-12 16:00:44 +0530

    Testm

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Skills</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
+        {/* <li><a href="#testimonials">Testimonials</a></li> */}
         <li><a href="#contact">Contact</a></li>
       </ul>
       <div className="footer__socials">
